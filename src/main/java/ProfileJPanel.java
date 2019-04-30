7e6c71ef04c7b178d4c769c98fba2983eb64a9a2
Alexander Kardapolov <akardapolov@gmail.com>
2019-05-01 01:39:40 +0600

    Annotations

--- src/main/java/ProfileJPanel.java
+++ src/main/java/ProfileJPanel.java
@@ -1,12 +1,24 @@
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.swing.*;
+import java.io.InputStream;
 
 @Singleton
 public class ProfileJPanel extends JPanel {
 
     @Inject
     public ProfileJPanel(){
+
         System.out.println("Create Profile JPanel");
+
+
+        InputStream labelsStream = Labels.class.getClassLoader().getResourceAsStream("messages.properties");
+        if (labelsStream == null){
+            System.out.println("null here");
+        } else {
+            System.out.println("not null");
+        }
+
+        System.out.println(Labels.getLabel("gui.connection.connection"));
     }
 }
